c482e2f64a5453de4279d2618719ed8da263626e
Travis Llado <travisllado@flqwhgads-MacBook.local>
2017-02-05 20:57:34 -0600

    removed separate vision folder

--- Dreamer Vision/Test Builds/t0003/test.py
+++
@@ -1,9 +0,0 @@
-gazeKey = 0xA7
-gazeFocus = 0x80
-gazePitch = 0x80
-gazeYaw = 0x80
-
-newCommand = gazeKey*0x1000000 + gazeFocus*0x10000 + gazePitch*0x100 + gazeYaw
-newMessage = str(newCommand) + "\r"
-
-print newMessage
