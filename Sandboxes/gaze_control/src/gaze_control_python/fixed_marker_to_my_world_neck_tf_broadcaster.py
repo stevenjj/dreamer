b875fb96027b70accf1aa95d8eb662b89eb3c14d
Steven Jens Jorgensen <stevenjensj@gmail.com>
2017-03-06 20:22:48 -0600

    added kinect to dreamer marker tf

--- Sandboxes/gaze_control/src/gaze_control_python/fixed_marker_to_my_world_neck_tf_broadcaster.py
+++ Sandboxes/gaze_control/src/gaze_control_python/fixed_marker_to_my_world_neck_tf_broadcaster.py
@@ -6,6 +6,8 @@ import tf
 
 from geometry_msgs.msg import TransformStamped
 
+# Center of Marker to Center of Neck
+
 if __name__ == '__main__':
     rospy.init_node('fixed_marker_to_my_world_neck_tf_broadcaster')
     br = tf.TransformBroadcaster()
@@ -16,6 +18,6 @@ if __name__ == '__main__':
         br.sendTransform((0.4, 0.0, 0.08),
                          (0, 0.0, 0.0, 1.0),
                          rospy.Time.now(),
-                         "my_world_neck",
-                         "dreamer_marker")
+                         "/my_world_neck",
+                         "/dreamer_marker")
         rate.sleep()
