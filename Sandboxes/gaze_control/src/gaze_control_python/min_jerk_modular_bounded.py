6a749d348bd8c758fe5b069f73f2521a29699c06
bcshang <bcshang@utexas.edu>
2017-04-03 13:53:51 -0500

    Added calculations for all parameters (velocity, acceleration), Tested code and looks pretty smooth

--- Sandboxes/gaze_control/src/gaze_control_python/min_jerk_modular_bounded.py
+++ Sandboxes/gaze_control/src/gaze_control_python/min_jerk_modular_bounded.py
@@ -10,8 +10,6 @@ TODO:
 
 '''
 Questions:
-	- Do I need to bound the velocity/acceleration?
-	- Should I return none if given bad values like t = -5?
 '''
 
 ### Class Waypoint
@@ -26,7 +24,7 @@ class Waypoint():
 
 
 ### Class MinimumJerk
-#		Declare this class 
+#		Declare this class with a standard vertical array of waypoints
 #		Note that in our implementation of Minimum Jerk, Dt takes a different role from in Waypoint
 #		It becomes absolute time rather than a change in time, starting from to = 0
 class MinimumJerk():
@@ -82,43 +80,69 @@ class MinimumJerk():
 			self.coeffs[i-1] = self.point_min_jerk_coeffs(self.waypoint_list[i-1], self.waypoint_list[i])
 
 	 
-	# Function: Returns the position of the functions given a time
-	# Input: None
-	# Return: None
-	def get_position(self, time):
+	# Function: Returns the position, velocty, and acceleration of the functions given a time
+	# Input: time
+	# Return: Numpy array of [position, velocity, acceleration]
+	def get_all_s(self, time):
 		length = len(self.waypoint_list)
 		if(time < 0):
 			return None
 		elif(time > self.waypoint_list[length-1].Dt):
-			return self.waypoint_list[length-1].s[0] #Returns the final position if out of time range
+			return np.array([self.waypoint_list[length-1].s[0], self.waypoint_list[length-1].s[1], self.waypoint_list[length-1].s[2]])
 		else:
 			for i in range(1, length):
 				if (self.waypoint_list[i-1].Dt <= time <= self.waypoint_list[i].Dt):
 					# This is ugly but is essentially our 5th order equation
-					return (self.coeffs[i-1][0] +
+					return np.array([ 
+							(self.coeffs[i-1][0] +
 						    self.coeffs[i-1][1] * time +
 						    self.coeffs[i-1][2] * (time**2) +
 						    self.coeffs[i-1][3] * (time**3) +
 						    self.coeffs[i-1][4] * (time**4) +
 						    self.coeffs[i-1][5] * (time**5)	)
+							,
+							(self.coeffs[i-1][1] +
+						    self.coeffs[i-1][2] * 2 * (time) +
+						    self.coeffs[i-1][3] * 3 * (time**2) +
+						    self.coeffs[i-1][4] * 4 * (time**3) +
+						    self.coeffs[i-1][5] * 5 * (time**4)	)
+						    ,
+						    (self.coeffs[i-1][2] * 2 +
+						    self.coeffs[i-1][3] * 6 * (time) +
+						    self.coeffs[i-1][4] * 12 * (time**2) +
+						    self.coeffs[i-1][5] * 20 * (time**3))
+						    ])
 
+	# Helper functions in case you only want one value
+	def get_position(self, time):
+		return (self.get_all_s(time))[0]
 
+	def get_velocity(self, time):
+		return (self.get_all_s(time))[1]
+
+	def get_acceleration(self, time):
+		return (self.get_all_s(time))[2]
 
 # Sample Code:
-# 	Will move through 4 points over the course of 1.5s
-
+f = open('output.txt', 'w')
 # First define the points as an array pf Waypoint classes
 x_coord = []
-x_coord.append(Waypoint(0, 0, 0, 0))
-x_coord.append(Waypoint(10, 5, 0, .5))
-x_coord.append(Waypoint(20, 0, 0, .5))
-x_coord.append(Waypoint(30, 0, 0, .5))
+x_coord.append(Waypoint(65, 65, 4, 0))
+x_coord.append(Waypoint(12, 12, 65, 1))
+x_coord.append(Waypoint(54, 73, -51, 1))
+x_coord.append(Waypoint(78, -87, 7, 1))
+x_coord.append(Waypoint(43, 12, -16, 1))
+x_coord.append(Waypoint(61, 93, -5, 1))
 
 # Declare a MinimumJerk Class based on the previous array
 test_move = MinimumJerk(x_coord)
-print "Coefficients:"
-print test_move.coeffs
 
 # Coefficients are automatically calculated, so we can simply input a time
-print "\nLocation at t = 1.2"
-print test_move.get_position(1.2)
+val = np.arange(0, 5.05, 0.05)
+for i in range(0, 101):
+	f.write( str((test_move.get_all_s(val[i]))[0]) )
+	f.write("\t")
+	f.write( str((test_move.get_all_s(val[i]))[1]) )
+	f.write("\t")
+	f.write( str((test_move.get_all_s(val[i]))[2]) )
+	f.write("\n")
