surprised_no doesn't hit joint limits but also doesn't return to home configuration. Fixed head rolling calculation and head translation

Sandboxes/gaze_control/src/gaze_control_python/dreamer_controller.py
@@ -108,14 +108,13 @@ def calc_smooth_desired_orientation(x_gaze_loc, p_cur, Q_cur, Q_init, scaling, o
     #   let phi be the total roll in radians. set theta = phi*s(t) with s(t) being the minimum jerk scaling
     #    R_desired = R_desired*Rot(world_x_hat, theta) 
 
-    if(tilt == 0):
-        return R_desired
     
     # So tilting
     phi = tilt
     theta = phi * scaling
     head_tilt = Rot(x_world_hat, theta)
-    return R_desired + head_tilt
+
+    return np.dot(R_desired, head_tilt) 
     # print 'Desired Orientation '
     # print 'x_hat', x_hat_d.T
     # print 'y_hat', y_hat_d.T
@@ -600,8 +599,9 @@ class Controller():
         # A variable within the min_jerk will tell whether or not to move the head with the point or just the gaze location
         # TODO: Do proper calculations for moving the head orientation
         if(self.piecewise_func_head.get_pull(t)[0]):
-            # The following line will only cause an x translation for a certain gaze point
-            dx_head = np.concatenate( (dx_head, np.array([xyz_loc_dif[0], 0, -xyz_loc_dif[0]]) ),  axis=0)
+            # The following line will translate the figure based on how the x point moves
+            # z position on a circle moves by r - sqrt(r**2 - x**2)
+            dx_head = np.concatenate( (dx_head, np.array([xyz_loc_dif[0], 0, hk.Head_Kinematics().l1 - np.sqrt(hk.Head_Kinematics().l1**2 - xyz_loc_dif[0]**2)]) ),  axis=0)
         else:
             # The following line causes no head translation
             dx_head = np.concatenate( (dx_head, np.array([0, 0, 0]) ),  axis=0)
@@ -640,7 +640,7 @@ class Controller():
 
         HEAD = 1
         EYES = 2
-        PRIORITY = EYES #
+        PRIORITY = HEAD #
 
         if (PRIORITY == HEAD): 
             dx1 = dx_head

Sandboxes/gaze_control/src/gaze_control_python/min_jerk_coordinates.py
@@ -168,6 +168,7 @@ def test_script(tilt):
 # Inputs: Eye gaze length in meters
 # Outputs: Minimum Jerk functions for head and eyes
 # Notes:	Assumes a home configuration
+#			TODO: Does not return to perfect home configuration
 #			Motion order:
 # 						1. Initial gaze point
 # 						2. Head back
@@ -180,31 +181,33 @@ def test_script(tilt):
 def surprised_no(gaze_length):
 	head_min_jerk = None
 	eyes_min_jerk = None
-	time = 7.0
+	time = 7.5
 	no_distance = .3
-	head_back = .04
+	# 40% of maxmimum joint movement
+	head_back = np.sin(.26)*hk.Head_Kinematics().l1*.4
 	# Head will move according to above
 	head_x = []
 	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2, 0, 0, 0))
-	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2 - head_back, 0, 0, 1.0, True))
-	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2 - head_back, 0, 0, 5.0))
-	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2, 0, 0, 1.0, True))
+	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2 - head_back, 0, 0, .25, True))
+	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2 - head_back, 0, 0, 5.25))
+	head_x.append(single.Waypoint(gaze_length + hk.Head_Kinematics().l2, 0, 0, 2.0, True))
+
 	
 	head_y = []
 	head_y.append(single.Waypoint(0, 0, 0, 0))
-	head_y.append(single.Waypoint(0, 0, 0, 2.0))
+	head_y.append(single.Waypoint(0, 0, 0, 1.5))
 	head_y.append(single.Waypoint(no_distance, 0, 0, .25))
 	head_y.append(single.Waypoint(-no_distance, 0, 0, .5))
 	head_y.append(single.Waypoint(no_distance, 0, 0, .5))
 	head_y.append(single.Waypoint(-no_distance, 0, 0, .5))
 	head_y.append(single.Waypoint(0, 0, 0, .25))
-	head_y.append(single.Waypoint(0, 0, 0, 3.0))
+	head_y.append(single.Waypoint(0, 0, 0, 4.0))
 	
 	head_z = []
 	head_z.append(single.Waypoint(hk.Head_Kinematics().l1, 0, 0, 0))
-	head_z.append(single.Waypoint(hk.Head_Kinematics().l1 + head_back*4, 0, 0, 1.0))
-	head_z.append(single.Waypoint(hk.Head_Kinematics().l1 + head_back*4, 0, 0, 5.0))
-	head_z.append(single.Waypoint(hk.Head_Kinematics().l1, 0, 0, 1.0))
+	head_z.append(single.Waypoint(hk.Head_Kinematics().l1 + head_back * 3, 0, 0, .25))
+	head_z.append(single.Waypoint(hk.Head_Kinematics().l1 + head_back * 3, 0, 0, 5.25))
+	head_z.append(single.Waypoint(hk.Head_Kinematics().l1, 0, 0, 2.0))
 	
 
 	x_coord = single.MinimumJerk(head_x)
@@ -224,7 +227,7 @@ def surprised_no(gaze_length):
 	eyes_y.append(single.Waypoint(0, 0, 0, time))
 	eyes_z = []
 	eyes_z.append(single.Waypoint(hk.Head_Kinematics().l1, 0, 0, 0))
-	eyes_z.append(single.Waypoint(hk.Head_Kinematics().l1+head_back, 0, 0, time))
+	eyes_z.append(single.Waypoint(hk.Head_Kinematics().l1, 0, 0, time))
 
 	x_coord = single.MinimumJerk(eyes_x)
 	y_coord = single.MinimumJerk(eyes_y)